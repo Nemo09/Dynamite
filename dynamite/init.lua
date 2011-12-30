fdcfcc184c81b6820658742d92ecf233cf1b0a1d
Nemo 08 <bigblacklazycat@gmail.com>
2011-12-30 23:47:19 +0400

    folders added

---
+++ dynamite/init.lua
@@ -0,0 +1,214 @@
+--[[
+
+Author: Nemo08
+
+mod:dynamite v 0.0.3
+
+
+]]--
+
+--[ ***********************   coal dust  ***************** ]--
+
+	minetest.register_craft({
+		output = 'craft "dynamite:coal_dust" 1',
+		recipe = {
+			{'craft "default:coal_lump"'},
+			{'craft "default:coal_lump"'},
+			}
+	})
+
+	minetest.register_craftitem("dynamite:coal_dust", {
+	    image = "coal_dust.png",
+	    stack_max = 10,
+	    dropcount = 1,
+	    liquids_pointable = false,
+	    on_place_on_ground = minetest.craftitem_place_item,
+	})
+
+  DYNAMITE_TABLE = {}
+
+	explode_area = function (posd, half_size,change_node_list) --[[,callback_node_list]]-- 
+	       --[[
+			change list format:             список имен нод, которые изменяются взрывом
+				{node_name1 = node_name1x, node_name2 = node_name2x, .. }
+
+			callback node list format:	список имен нод, при попадании на которые вызывается коллбек
+				{{{node_name1, node_name2, .. }, callback_function(finded_node_pos) }, ..}
+		]]--
+
+		minetest.env:remove_node(posd)
+				
+		local rnd = 35
+
+
+
+		for x = -half_size, half_size do
+		for y = -half_size, half_size do
+		for z = -half_size, half_size do
+
+			dynpos = {x = posd.x+x,y = posd.y+y,z = posd.z+z}
+			target_node = minetest.env:get_node(dynpos)
+
+		
+				local explode_this = true
+
+				--check explosible а не динамит ли тут?
+				for i, i_state in pairs(DYNAMITE_TABLE) do
+					if (target_node.name == i)or(target_node.name == (i .. "_fired")) then
+						minetest.env:add_node(dynpos, {name= (i .. "_fired")})
+						explode_this = false    -- we not explose
+						break
+					end
+				end
+
+				--check excluse
+				if change_node_list ~= nil then
+				for i, i_state in pairs(change_node_list) do
+					if target_node.name == i then
+						minetest.env:add_node(dynpos, {name= (i_state)})
+						explode_this = false    -- we not explose
+						break
+					end
+				end
+				end
+				
+				--[[			не убирать!!!!!!	
+				--check callback
+				if callback_node_list ~= nil then
+				for i, i_state in ipairs(callback_node_list) do
+					if table.getn(i_state) ~= 0 then
+					for j, j_state in ipairs(i_state) do
+						local callb_func = j_state
+						if table.getn(j_state) ~= 0 then
+						for k, k_state in ipairs(j_state) do
+							if target_node.name == k_state then
+								callb_func(dynpos)	--find callback
+							end
+						end
+						end
+					end
+					end
+				end
+				end
+				]]--
+				
+				if target_node.name == "default:tree" then
+					local rnd_base = 0.04 + half_size * 0.01
+					for i = 1, 4 do
+					  wood_pos = {	x=(dynpos.x + (rnd_base/2)-(math.random(1,100)*rnd_base)), 
+							y=(dynpos.y + (rnd_base/2) - (math.random(1,100)*rnd_base)), 
+							z=(dynpos.z + (rnd_base/2) -(math.random(1,100)*rnd_base))}
+						target_wood_node = minetest.env:get_node(wood_pos)
+						if target_wood_node.name == "air" then 
+							if (math.random(1,100) < 40) then
+								minetest.env:add_item(wood_pos, 'craft "default:stick" 1')
+							else
+								minetest.env:add_item(wood_pos, 'craft "moarcraft:ash" 1')
+							end
+						end
+					end
+				end
+
+				if explode_this then
+					minetest.env:remove_node(dynpos)
+					local rnd = math.random(1,100)
+					if rnd < 35 then 
+						minetest.env:add_node(dynpos, {name="nparticle:fire_cloud1"})
+					end				
+				end
+
+		end
+		end
+		end
+	end
+
+
+	register_dynamite = function (dyn_node_name, dyn_image, dyn_image_fired, explode_cube_half_size, dyn_interval, dyn_chance, dyn_lightness, dyn_craft_recipe,
+				      change_node_list,callback_node_list)
+		
+		DYNAMITE_TABLE["dynamite:dyn_" .. dyn_node_name] = tostring(explode_cube_half_size)
+	
+		minetest.register_abm({
+			nodenames 	= {"dynamite:dyn_" .. dyn_node_name .. "_fired"},
+			interval 	= dyn_interval,
+			chanse		= dyn_chance,
+			action 		= function(pos, node, _, __)							
+						explode_area(pos, explode_cube_half_size,change_node_list,callback_node_list)
+					  end
+		})
+	
+
+		minetest.register_node("dynamite:dyn_" .. dyn_node_name , {
+			drawtype = "plantlike",
+       			tile_images = {dyn_image},
+			inventory_image = dyn_image,
+			paramtype = "light",
+			is_ground_content = true,
+			walkable = false,
+			sunlight_propagates = true,
+			--light_source = dyn_lightness,
+			material = minetest.digprop_constanttime(0.4),
+       			dug_item = 'craft "dynamite:dyn_' .. dyn_node_name .. '_craft" 1',
+		})
+
+		minetest.register_node("dynamite:dyn_" .. dyn_node_name .. "_fired", {
+			drawtype = "plantlike",
+       			tile_images = {dyn_image_fired},
+			inventory_image = dyn_image,
+			paramtype = "light",
+			is_ground_content = true,
+			walkable = false,
+			sunlight_propagates = true,
+			light_source = dyn_lightness,
+			material = minetest.digprop_constanttime(0.4),
+       			dug_item = 'craft "dynamite:dyn_' .. dyn_node_name .. '_craft" 1',
+		})  
+
+
+		minetest.register_craft({
+			output = 'craft "dynamite:dyn_' .. dyn_node_name .. '_craft" 1',
+			recipe = dyn_craft_recipe,
+		})
+	
+		minetest.register_craftitem("dynamite:dyn_" .. dyn_node_name .. "_craft", {
+		    image = dyn_image,
+		    stack_max = 10,
+		    usable = true,
+		    dropcount = 1,
+		    liquids_pointable = false,
+	
+		    on_place_on_ground = function (item, placer, pos)
+			minetest.env:add_node(pos, {name="dynamite:dyn_" .. dyn_node_name})
+		        return true	
+		    end,
+
+		    on_use = function(item, player, pointed_thing)
+			if pointed_thing.type == "node" then		
+				minetest.env:add_node(pointed_thing.above, {name="dynamite:dyn_" .. dyn_node_name .. "_fired"})
+				return true	
+			end
+	
+		        return false
+		    end,
+
+		})
+		return true
+	end
+
+--	function register_dynamite (dyn_node_name, dyn_image, dyn_image_fired, explode_cube_half_size, dyn_interval, dyn_chance, dyn_lightness, dyn_craft_recipe)
+	
+	local nnn = register_dynamite("small","dynamite_small.png","dynamite_small_fired.png", 2, 5, 0.8, 8,
+					{{'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'}},
+					{["default:stone"] = "default:cobble",["default:sand"] = "default:sand"})
+
+	local nnn = register_dynamite("normal","dynamite.png","dynamite_fired.png", 3, 5, 0.8, 10,
+					{{'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'},
+					 {'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'}},
+	  	 			{["default:stone"] = "default:gravel",["default:sand"] = "default:sand"})
+	
+	local nnn = register_dynamite("big","dynamite_big.png","dynamite_big_fired.png", 4, 5, 0.8, 12,
+					{{'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'},
+					 {'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'},
+					 {'node "default:papyrus"','craft "dynamite:coal_dust"','node "default:papyrus"'}})
+
+print("[Dynamite] Loaded!")
---
+++ nparticle/init.lua
@@ -0,0 +1,285 @@
+--[[
+
+Author: Nemo08
+
+mod: nparticle v 0.0.1
+
+]]--
+	math.randomseed(os.time())
+
+--[ ************************************* FIRE************************************]--
+	minetest.register_node("nparticle:fire_cloud1", {
+		drawtype = "plantlike",
+        	tile_images = {"fire_cloud_1.png","fire_cloud_1.png","fire_cloud_1.png","fire_cloud_1.png","fire_cloud_1.png","fire_cloud_1.png"},
+		paramtype = "light",
+		walkable = false,
+		pointable = false,
+		diggable = false,
+		buildable_to = false,
+		alpha = 230,		
+		sunlight_propagates = true,
+		light_source = 25,
+		damage_per_second = 4*2,
+		post_effect_color = {a=192, r=255, g=64, b=0},
+	})
+
+	minetest.register_node("nparticle:fire_cloud2", {
+		drawtype = "plantlike",
+        	tile_images = {"fire_cloud_2.png","fire_cloud_2.png","fire_cloud_2.png","fire_cloud_2.png","fire_cloud_2.png","fire_cloud_2.png"},
+		paramtype = "light",
+		walkable = false,
+		pointable = false,
+		diggable = false,
+		buildable_to = false,
+		alpha = 200,
+		sunlight_propagates = true,
+		light_source = 15,
+		damage_per_second = 2*2,
+		post_effect_color = {a=192, r=255, g=64, b=0},
+	})
+
+
+
+--[ ************************************* FIRE ABMs ***************************************]--
+
+
+	FIRE1_INTERVAL 	= 5
+	FIRE1_CHANSE	= 0.7
+	FIRE1_MOVE_CUBE = 1    --пределы распространения
+	FIRE_MOVE_CHANSE = 3  --шанс переместиться [1..100]
+	FIRE_CHANGE_CHANSE = 70  --шанс потерять температуру [1..100]
+
+	minetest.register_abm({
+		nodenames 	= {"nparticle:fire_cloud1"},
+		interval 	= FIRE1_INTERVAL,
+		chanse		= FIRE1_CHANSE,
+		action 		= function(pos, node, _, __)
+						
+					for x = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+					for y = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+					for z = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+						local dynpos 	= {x = pos.x+x,y = pos.y+y,z = pos.z+z}
+						local targetnode = minetest.env:get_node(dynpos)
+						local rand_move = math.random(1,100)
+						if targetnode.type == "node" then
+						if ((targetnode.name == "air")or 
+						   (strfind(targetnode.name,"nparticle:dust_cloud") ~= nil) or
+						   (strfind(targetnode.name,"nparticle:fire_cloud") ~= nil))and
+						   (rand_move <= FIRE_MOVE_CHANSE) 
+						then 
+							minetest.env:add_node(dynpos,{name = "nparticle:fire_cloud2"})
+						end
+						end
+
+					end
+					end
+					end
+
+					local rand_change = math.random(1,100)
+					if rand_change <= FIRE_CHANGE_CHANSE then
+						minetest.env:add_node(pos,{name = "nparticle:fire_cloud2"})
+					else
+						minetest.env:remove_node(pos)
+					end
+				  end
+	})
+
+	minetest.register_abm({
+		nodenames 	= {"nparticle:fire_cloud2"},
+		interval 	= FIRE1_INTERVAL ,
+		chanse		= FIRE1_CHANSE  ,
+		action 		= function(pos, node, _, __)
+						
+					for x = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+					for y = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+					for z = -FIRE1_MOVE_CUBE,FIRE1_MOVE_CUBE do
+						local dynpos 	= {x = pos.x+x,y = pos.y+y,z = pos.z+z}
+						local targetnode = minetest.env:get_node(dynpos)
+						local rand_move = math.random(1,100)
+
+						if targetnode.type == "node" then
+						if ((targetnode.name == "air")or 
+						   (strfind(targetnode.name,"nparticle:dust_cloud") ~= nil) or
+						   (strfind(targetnode.name,"nparticle:fire_cloud") ~= nil))and
+						   (rand_move <= (FIRE_MOVE_CHANSE / 3)) 
+						then 
+							minetest.env:add_node(dynpos,{name = "nparticle:fire_cloud2"})
+							minetest.env:remove_node(pos)
+						end
+						end
+
+					end
+					end
+					end
+
+						minetest.env:add_node(pos,{name = "nparticle:dust_cloud3"})
+
+				  end
+	})
+
+--[ ************************************* DUST ************************************]--
+	
+
+	DUST_INTERVAL 	= 3
+	DUST_CHANSE	= 0.5
+	DUST_MOVE_CUBE = 2    --пределы распространения
+	DUST_BREAK_CHANSE = 50  -- [1..100]  шанс частицы "разлететься"
+	DUST_MOVE_NODE_CHANSE = 50  -- [1..100]  шанс разлетевшейся частицы попасть в конкретную ячейку
+	DUST_DENSITY_MAX = 230  -- максимальное значение плотности пыли
+	DUST_COLLAPSE = 70    -- предел плотности, после которого частица пыли исчезает
+	
+		
+for k=0,4 do
+	minetest.register_node("nparticle:dust_cloud" .. tostring(k), {
+		drawtype = "glasslike",
+        	tile_images = {"dust" .. tostring(k) .. ".png"},
+		paramtype="light",
+		walkable = false,
+		pointable = false,
+		diggable = false,
+		buildable_to = false,
+		sunlight_propagates = true,
+		is_ground_content = true,
+		material = minetest.digprop_glasslike(1.0),
+		post_effect_color = {a=(50 + k*50), r=48, g=48, b=48},
+		alpha = 50 + k*50,
+		
+	})
+
+
+
+	minetest.register_abm({
+		nodenames 	= {"nparticle:dust_cloud" .. tostring(k)},
+		interval 	= DUST_INTERVAL ,
+		chanse		= DUST_CHANSE  ,
+		action 		= function(pos, node, _, __)
+		
+					local rand_move = math.random(1,100)
+					local this_node = minetest.env:get_node(pos)
+					local kp = k
+					
+					if rand_move <= DUST_BREAK_CHANSE then
+						
+						for x = -DUST_MOVE_CUBE,DUST_MOVE_CUBE do
+						for y = -DUST_MOVE_CUBE,DUST_MOVE_CUBE do
+						for z = -DUST_MOVE_CUBE,DUST_MOVE_CUBE do
+							local rand_move = math.random(1,100)+y*20
+							if rand_move <= DUST_MOVE_NODE_CHANSE then
+								local dynpos 	= {x = pos.x+x,y = pos.y+y,z = pos.z+z}
+								local targetnode = minetest.env:get_node(dynpos)
+																									
+								if (targetnode.name == "air") and  ((kp-1)>1) then
+									minetest.env:add_node(dynpos, {name="nparticle:dust_cloud" .. tostring(k-1)})
+								end
+								
+								if targetnode.type == "node" then
+									if strfind(targetnode.name,"nparticle:dust_cloud")~=nil then
+										local target_k = (targetnode.alpha-50)/50
+										local new_k = (target_k + kp)
+										if new_k >=4 then 
+											minetest.env:add_node(dynpos, {name="nparticle:dust_cloud4"})
+										else
+											minetest.env:add_node(dynpos, {name="nparticle:dust_cloud" .. tostring(new_k)})
+										end
+									end
+								end
+							end
+						end
+						end
+						end
+							
+							minetest.env:remove_node(pos)
+						
+					end
+					
+					if kp <=0 then
+						minetest.env:remove_node(pos)
+					end
+
+				  end
+	})
+end	
+
+
+--[ ************************************* SMOKE ************************************]--
+	
+
+	SMOKE_INTERVAL 	= 2
+	SMOKE_CHANSE	= 0.9
+	SMOKE_MOVE_CUBE = 1    --пределы распространения
+	SMOKE_BREAK_CHANSE = 15  -- [1..100]  шанс частицы "разлететься"
+	SMOKE_MOVE_NODE_CHANSE = 30  -- [1..100]  шанс разлетевшейся частицы попасть в конкретную ячейку
+	SMOKE_COLLAPSE = 70    -- предел плотности, после которого частица пыли исчезает
+	
+		
+for k=0,4 do
+	minetest.register_node("nparticle:smoke_cloud" .. tostring(k), {
+		drawtype = "glasslike",
+        	tile_images = {"smoke" .. tostring(k) .. ".png"},
+		paramtype="light",
+		walkable = false,
+		pointable = false,
+		diggable = false,
+		buildable_to = false,
+		sunlight_propagates = true,
+		is_ground_content = true,
+		material = minetest.digprop_glasslike(1.0),
+		post_effect_color = {a=(50 + k*50), r=255, g=255, b=255},
+		alpha = 50 + k*50,
+		
+	})
+
+	minetest.register_abm({
+		nodenames 	= {"nparticle:smoke_cloud" .. tostring(k)},
+		interval 	= SMOKE_INTERVAL ,
+		chanse		= SMOKE_CHANSE  ,
+		action 		= function(pos, node, _, __)
+		
+					local this_node = minetest.env:get_node(pos)
+					local kp = k
+					local rand_break = math.random(1,100)
+					
+					if rand_break <= SMOKE_BREAK_CHANSE then
+						for x = -SMOKE_MOVE_CUBE,SMOKE_MOVE_CUBE do
+						for y = 0,SMOKE_MOVE_CUBE do
+						for z = -SMOKE_MOVE_CUBE,SMOKE_MOVE_CUBE do
+							local rand_move = math.random(1,100-y*15)
+							if rand_move <= SMOKE_MOVE_NODE_CHANSE then
+								local dynpos 	= {x = pos.x+x,y = pos.y+y,z = pos.z+z}
+								local targetnode = minetest.env:get_node(dynpos)
+																									
+								if (targetnode.name == "air")and  ((kp-1) >1) then
+										minetest.env:add_node(dynpos, {name="nparticle:smoke_cloud" .. tostring(k-1)})
+								end
+								minetest.env:remove_node(pos)
+							end							
+						end
+						end
+						end
+					else
+						local dynpos 	= {x = pos.x,y = pos.y+1,z = pos.z}
+						local targetnode = minetest.env:get_node(dynpos)
+						if (targetnode.name ~= "air") then
+							dynpos 	= {x = (pos.x+math.random(-1,1)),y = pos.y,z = (pos.z+math.random(-1,1))}
+							local targetnode = minetest.env:get_node(dynpos)
+							if (targetnode.name ~= "air") then
+								local dynpos 	= pos	
+							end
+						end
+	
+						if (kp>0) then
+							local rand_break = math.random(1,100)
+							if rand_break < 15 then
+								minetest.env:add_node(dynpos, {name="nparticle:smoke_cloud" .. tostring(k-1)})
+							else
+								minetest.env:add_node(dynpos, {name="nparticle:smoke_cloud" .. tostring(k)})
+							end
+						end
+						minetest.env:remove_node(pos)
+					end
+					
+				  end
+	})
+end		
+
+print("[nParticles] Loaded!")	
